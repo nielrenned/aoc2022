823807f2e27efea3558ec790dfda6605ed41b4bd
Jacob Dennerlein <nielrenned@gmail.com>
2022-12-01 11:07:56 -0500

    Add templates.

---
+++ day19.py
@@ -0,0 +1,30 @@
+DAY = 19
+RAW_INPUT = None
+INPUT = None
+
+def load_input(use_test_input=False):
+    global RAW_INPUT
+    path = f'inputs/day{DAY:02}.txt'
+    if use_test_input:
+        path = f'inputs/day{DAY:02}_test.txt'
+    with open(path) as f:
+        RAW_INPUT = f.read()
+
+def parse_input():
+    global INPUT
+    pass
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+def main():
+    load_input()
+    parse_input()
+    print('PART 1:', part1())
+    # print('PART 2:', part2())
+
+if __name__ == "__main__":
+    main()
