28486cd97f87920bd21b8c626098b036d1ddcd4a
Jacob Dennerlein <nielrenned@gmail.com>
2022-12-02 11:10:45 -0500

    Finished Day 2!

--- day02.py
+++ day02.py
@@ -12,19 +12,40 @@ def load_input(use_test_input=False):
 
 def parse_input():
     global INPUT
-    pass
+    INPUT = []
+    for line in RAW_INPUT.split('\n')[:-1]:
+        INPUT.append(line.split(' '))
+
+# Each score is <move_value> + <outcome_value>.
+# Both indices are moves here, so move_value doesn't 
+# change between columns.
+PART1_SCORES = {'A': {'X': 1 + 3, 'Y': 2 + 6, 'Z': 3 + 0},
+                'B': {'X': 1 + 0, 'Y': 2 + 3, 'Z': 3 + 6},
+                'C': {'X': 1 + 6, 'Y': 2 + 0, 'Z': 3 + 3}}
 
 def part1():
-    pass
+    total_score = 0
+    for opp_move, my_move in INPUT:
+        total_score += PART1_SCORES[opp_move][my_move]
+    return total_score
+
+# Each score is <move_value> + <outcome_value>.
+# The second index is outcome here, so outcome_value
+# doesn't change between columns.
+PART2_SCORES = {'A': {'X': 3 + 0, 'Y': 1 + 3, 'Z': 2 + 6},
+                'B': {'X': 1 + 0, 'Y': 2 + 3, 'Z': 3 + 6},
+                'C': {'X': 2 + 0, 'Y': 3 + 3, 'Z': 1 + 6}}
 
 def part2():
-    pass
-
+    total_score = 0
+    for opp_move, outcome in INPUT:
+        total_score += PART2_SCORES[opp_move][outcome]
+    return total_score
 def main():
     load_input()
     parse_input()
     print('PART 1:', part1())
-    # print('PART 2:', part2())
+    print('PART 2:', part2())
 
 if __name__ == "__main__":
     main()
