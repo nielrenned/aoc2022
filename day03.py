f1e6f88df63a978e51ad7a7c7ac4782aa44e3afb
Jacob Dennerlein <nielrenned@gmail.com>
2022-12-03 08:58:13 -0500

    Finished Day 3!

--- day03.py
+++ day03.py
@@ -1,3 +1,5 @@
+import string
+
 DAY = 3
 RAW_INPUT = None
 INPUT = None
@@ -12,19 +14,32 @@ def load_input(use_test_input=False):
 
 def parse_input():
     global INPUT
-    pass
+    INPUT = RAW_INPUT.split('\n')[:-1]
+
+PRIORITY = {x: i + 1 for i, x in enumerate(string.ascii_letters)}
 
 def part1():
-    pass
+    total = 0
+    for sack in INPUT:
+        compartment1 = set(sack[:len(sack)//2])
+        compartment2 = set(sack[len(sack)//2:])
+        common_item = (compartment1 & compartment2).pop()
+        total += PRIORITY[common_item]
+    return total
 
 def part2():
-    pass
+    total = 0
+    for i in range(0, len(INPUT), 3):
+        sack1, sack2, sack3 = map(set, INPUT[i:i+3])
+        badge_item = (sack1 & sack2 & sack3).pop()
+        total += PRIORITY[badge_item]
+    return total
 
 def main():
     load_input()
     parse_input()
     print('PART 1:', part1())
-    # print('PART 2:', part2())
+    print('PART 2:', part2())
 
 if __name__ == "__main__":
     main()
