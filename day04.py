2db7f4f8c5d90aed6ee9a4342be7b36894b86e56
Jacob Dennerlein <nielrenned@gmail.com>
2022-12-04 10:15:07 -0500

    Finished Day 4!

--- day04.py
+++ day04.py
@@ -12,19 +12,34 @@ def load_input(use_test_input=False):
 
 def parse_input():
     global INPUT
-    pass
+    INPUT = []
+    for line in RAW_INPUT.split('\n')[:-1]:
+        s1, s2 = line.split(',')
+        s1_start, s1_end = map(int, s1.split('-'))
+        s2_start, s2_end = map(int, s2.split('-'))
+        INPUT.append(((s1_start, s1_end), (s2_start, s2_end)))
 
 def part1():
-    pass
+    count = 0
+    for (s1_start, s1_end), (s2_start, s2_end) in INPUT:
+        if s1_start <= s2_start and s2_end <= s1_end or \
+           s2_start <= s1_start and s1_end <= s2_end:
+            count += 1
+    return count
 
 def part2():
-    pass
+    count = 0
+    for (s1_start, s1_end), (s2_start, s2_end) in INPUT:
+        if s1_start <= s2_start and s2_start <= s1_end or \
+           s2_start <= s1_start and s1_start <= s2_end:
+            count += 1
+    return count
 
 def main():
     load_input()
     parse_input()
     print('PART 1:', part1())
-    # print('PART 2:', part2())
+    print('PART 2:', part2())
 
 if __name__ == "__main__":
     main()
