9497c026e7759cc8e005bf0ab9e53f61ff9f2d87
Jacob Dennerlein <nielrenned@gmail.com>
2022-12-10 10:35:13 -0500

    Finished Day 10!

--- day10.py
+++ day10.py
@@ -12,19 +12,57 @@ def load_input(use_test_input=False):
 
 def parse_input():
     global INPUT
-    pass
+    INPUT = []
+    for line in RAW_INPUT.split('\n')[:-1]:
+        INPUT.append(line.split())
+
+def process_instructions():
+    """Calculates the value of the X register during each clock cycle."""
+    clock = 0
+    X_values = [1]
+    X = 1
+    for instr in INPUT:
+        op = instr[0]
+        if op == 'noop':
+            X_values.append(X)
+            clock += 1
+        elif op == 'addx':
+            X_values.append(X)
+            amount = int(instr[1])
+            X += amount
+            X_values.append(X)
+            clock += 2
+
+    return X_values
 
 def part1():
-    pass
+    X_values = process_instructions()
+    return sum(X_values[i-1] * i for i in range(20, 221, 40))
 
 def part2():
-    pass
+    X_values = process_instructions()
+    screen = [' ']*240
+
+    for y in range(6):
+        for x in range(40):
+            clock = 40*y + x
+            X = X_values[clock]
+            if X-1 <= x <= X+1:
+                screen[clock] = '#'
+    
+    ret_string = '\n'
+    for y in range(6):
+        for x in range(40):
+            ret_string += screen[40*y+x] + ' '
+        ret_string += '\n'
+    
+    return ret_string[:-1]
 
 def main():
     load_input()
     parse_input()
     print('PART 1:', part1())
-    # print('PART 2:', part2())
+    print('PART 2:', part2())
 
 if __name__ == "__main__":
     main()
